d135512ec77245a3d4681a7ca5c0d00ce8c24f81
leikoz <53431749+leikoz@users.noreply.github.com>
2019-08-04 16:15:58 -0500

    Add files via upload

---
+++ 1000Cadenas/1000Cadenas/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int lenght;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                lenght = r.Next(2, 8);
+                string cadena = "";
+                int contadorC = 0;
+                int contadorV = 0;
+                string[] vocalRnd = { "a", "e", "i", "o", "u" };
+
+                for (int j = 0; j < lenght; j++) //97 - 122 : 97 101 105 111 117
+                {
+                    char letra = (char)r.Next(97, 122);
+
+                    if (letra != 97 && letra != 101 && letra != 105 && letra != 111 && letra != 117)
+                        contadorC++;
+                    else
+                        contadorV++;
+
+                    cadena += letra;
+
+                    if (contadorC > 4 && contadorV == 0)
+                    {
+                        cadena += vocalRnd[r.Next(0, 4)];
+                    }
+
+                }
+
+                Console.WriteLine(cadena);
+            }
+        }
+    }
+}
